d83963f9f653d2ded1b7113cfee12b857e95a871
SeryiBaran <seryibaranminepy@yandex.ru>
2022-03-09 16:35:22 +0300

    BugFix

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
 import '@fontsource/noto-sans';
+import Flickity from 'flickity';
+
+import 'flickity/dist/flickity.min.css';
+import '../scss/main.scss';
+
+
+let flkty = new Flickity('.photos', {
+  
+});
