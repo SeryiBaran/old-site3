8a7e35b97edd61df4ad59fff1c00fa35d1c9d4f7
SeryiBaran <seryibaranminepy@yandex.ru>
2022-06-16 15:48:46 +0300

    Remove swiper

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,3 @@
 import '@fontsource/noto-sans';
-import Swiper from 'swiper/bundle';
-import 'swiper/css/bundle';
-
-const photos = new Swiper('.photos', {
-	slidesPerView: 'auto',
-	centeredSlides: true,
-	spaceBetween: 30,
-	pagination: {
-		el: '.photos-pagination',
-		clickable: true,
-	},
-	navigation: {
-		nextEl: '.photos-button-next',
-		prevEl: '.photos-button-prev',
-	},
-});
 
 import '../scss/main.scss';
